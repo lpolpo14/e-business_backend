40e272d956b73b6c484e8cf8196be54f8c9841bf
Michael Favvas <122233869+lpolpo14@users.noreply.github.com>
2026-04-26 03:10:20 +0300

    Fixed address for CORS

--- d3fender/api/main.py
+++ d3fender/api/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:8080",
-        "https://your-spring-app.up.railway.app",
+        "https://d3fender.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
